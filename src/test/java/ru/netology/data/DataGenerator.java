f71c7cc7963cb4a05b789b9702109f8b8a8d4ffa
Maksim <maksimshalaev11@gmail.com>
2021-10-22 20:36:11 +0300

    Updated DataGenerator, Added RegistrationInfo

--- src/test/java/ru/netology/data/DataGenerator.java
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -7,30 +7,29 @@ import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public class DataGenerator {
+    private DataGenerator() {
+    }
 
-        private Faker faker;
-        private LocalDate today = LocalDate.now();
-        private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-
-        public String createDate(int plusDays) {
-            LocalDate newDate = today.plusDays(plusDays);
-            return formatter.format(newDate);
+    public static class Registration {
+        private Registration() {
         }
 
-        public String getCity() {
+        public static RegistrationInfo generate(String locale) {
+            Faker faker = new Faker(new Locale("ru"));
+            return new RegistrationInfo(
+                    faker.address().city(),
+                    faker.name().lastName() + " " + faker.name().firstName(),
+                    faker.numerify("+7##########"));
+        }
+
+        public static String generateDate(int days) {
+            return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        }
+
+        public static String getCity() {
             String[] myCityList = new String[]{"Абакан", "Анадырь", "Архангельск", "Астрахань", "Барнаул", "Владикавказ", "Горно-Алтайск", "Йошкар-Ола", "Казань", "Калининград", "Калуга", "Краснодар", "Магас", "Махачкала", "Нарьян-Мар", "Салехард", "Самара", "Саранск", "Саратов", "Хабаровск", "Ханты-Мансийск", "Южно-Сахалинск", "Великий Новгород", "Владивосток", "Владимир", "Вологда", "Рязань", "Биробиджан", "Чебоксары", "Москва", "Санкт-Петербург", "Ульяновск", "Симферополь", "Ростов-на-Дону"};
             int city = (int) Math.floor(Math.random() * myCityList.length);
             return myCityList[city];
         }
-
-
-        public String getName() {
-            faker = new Faker(new Locale("ru"));
-            return (faker.name().lastName()+(" ")+faker.name().firstName());
-        }
-
-        public String getPhone() {
-            faker = new Faker(new Locale("ru"));
-            return faker.phoneNumber().phoneNumber();
-        }
+    }
 }
