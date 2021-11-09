0c5abd51e54bae6d0e295268928721258ec8f14b
Maksim <maksimshalaev11@gmail.com>
2021-11-09 23:26:27 +0300

    Initial commit

--- src/test/java/ru/netology/data/DataGenerator.java
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -2,37 +2,39 @@ package ru.netology.data;
 
 import com.github.javafaker.Faker;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Locale;
-import java.util.Random;
+import java.util.*;
 
 public class DataGenerator {
     private DataGenerator() {
     }
 
-    public static class Registration {
-        private Registration() {
-        }
+    public static String getCity() {
+        List<String> cityList = Arrays.asList("Абакан", "Барнаул", "Нижний Новгород", "Ставрополь", "Ульяновск", "Кострома");
+        Random random = new Random();
+        String randomElement = cityList.get(random.nextInt(cityList.size()));
+        return randomElement;
+    }
 
+    public static String getDate() {
+        LocalDate date = LocalDate.now().plusDays(3);
+        return date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
 
-        public static RegistrationCard generate(String locale) {
-            Faker faker = new Faker(new Locale("ru"));
-            return new RegistrationCard(
-                    faker.address().city(),
-                    faker.name().lastName() + " " + faker.name().firstName(),
-                    faker.numerify("+7##########"));
-        }
+    public static String getNewDate() {
+        LocalDate date = LocalDate.now().plusDays(7);
+        return date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
 
-        public static String generateDate(int days) {
-            return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        }
+    public static String getName() {
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.name().lastName() + " " + faker.name().firstName();
+    }
 
-        public static String citiesTrue() {
-            Random random = new Random();
-            int rand = random.nextInt(5);
-            String cities[] = {"Москва", "Санкт-Петербург", "Челябинск", "Екатеринбург", "Казань"};
-            return cities[rand];
-        }
+    public static String getPhone() {
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.phoneNumber().cellPhone();
     }
 }
--- src/test/java/ru/netology/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/test/CardDeliveryTest.java
@@ -1,42 +1,76 @@
 package ru.netology.test;
 
-import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-import ru.netology.data.DataGenerator;
-import ru.netology.data.RegistrationCard;
 
-import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static ru.netology.data.DataGenerator.Registration.citiesTrue;
-import static ru.netology.data.DataGenerator.Registration.generateDate;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.netology.data.DataGenerator.*;
 
 public class CardDeliveryTest {
-    RegistrationCard info = DataGenerator.Registration.generate("ru");
+
+    public String city = getCity();
+    public String name = getName();
+    public String date = getDate();
+    public String newDate = getNewDate();
+    public String phone = getPhone();
+
+    @BeforeEach
+    void setUpAll() {
+        open("http://localhost:9999");
+    }
 
     @Test
-    void shouldGenerateTestData(){
+    void shouldPlanAndReplanMeeting() {
+        $("[data-test-id='city'] .input__control").setValue(city);
+        $(".menu-item_type_block").click();
+        $("[data-test-id='date'] .input__control").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        $("[data-test-id='date'] .input__control").setValue(date);
+        $("[data-test-id='name'] .input__control").setValue(name);
+        $("[data-test-id='phone'] .input__control").setValue(phone);
+        $("[data-test-id='agreement']").click();
+        $(byText("Запланировать")).click();
+        $(byText("Успешно!")).isDisplayed();
+        String firstDate = $(".notification__content").getText();
+        assertEquals("Встреча успешно запланирована на " + getDate(), firstDate);
 
-        open("http://localhost:9999");
-        $("[data-test-id='city'] input").setValue(citiesTrue());
-        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").setValue(generateDate(4));
-        $("[data-test-id='name'] input").setValue(info.getName());
-        $("[data-test-id='phone'] input").setValue(info.getPhone());
-        $("[class=checkbox__box]").click();
-        $("[class=button__text]").click();
-        $(withText("Успешно!")).shouldBe(Condition.appear);
-        $(".notification__content").shouldHave(Condition.text("Встреча успешно запланирована на " + generateDate(4)));
-        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").sendKeys(generateDate(6));
-        $(withText("Запланировать")).click();
-        $("[class=button__text]").click();
-        $("[data-test-id='replan-notification'] .notification__title").shouldBe(Condition.appear);
-        $(withText("Перепланировать")).click();
-        $(".notification__content").shouldHave(exactText("Встреча успешно запланирована на " + generateDate(6)));
+        $("[data-test-id='date'] .input__control").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        $("[data-test-id='date'] .input__control").setValue(newDate);
+        $(byText("Запланировать")).click();
+
+        $(byText("Перепланировать")).click();
+        $(".notification_visible").shouldHave(text("Успешно!"));
+        String secondDate = $("[data-test-id='success-notification'] .notification__content").getText();
+        assertEquals("Встреча успешно запланирована на " + getNewDate(), secondDate);
     }
 }
+
+
+//public class CardDeliveryTest {
+//    RegistrationCard info = DataGenerator.Registration.generate("ru");
+//
+//    @Test
+//    void shouldGenerateTestData(){
+//        open("http://localhost:9999");
+//        $("[data-test-id='city'] input").setValue(citiesTrue());
+//        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+//        $("[data-test-id='date'] input").setValue(generateDate(4));
+//        $("[data-test-id='name'] input").setValue(info.getName());
+//        $("[data-test-id='phone'] input").setValue(info.getPhone());
+//        $("[class=checkbox__box]").click();
+//        $("[class=button__text]").click();
+//        $(withText("Успешно!")).shouldBe(Condition.appear);
+//        $(".notification__content").shouldHave(Condition.text("Встреча успешно запланирована на " + generateDate(4)));
+//        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+//        $("[data-test-id='date'] input").sendKeys(generateDate(6));
+//        $(withText("Запланировать")).click();
+//        $("[class=button__text]").click();
+//        $("[data-test-id='replan-notification'] .notification__title").shouldBe(Condition.appear);
+//        $(withText("Перепланировать")).click();
+//        $(".notification__content").shouldHave(exactText("Встреча успешно запланирована на " + generateDate(6)));
+//    }
+//}
